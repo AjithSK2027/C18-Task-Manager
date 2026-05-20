e730bc9ea38d622e34b4029f352e5317711b311a
AjithSK2027 <adominic906@gmail.com>
2026-05-20 15:12:34 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,3 @@
-// ============================================================
-// C18 Task Manager — Frontend Script
-// IMPORTANT: Replace API_BASE with your deployed Apps Script URL
-// ============================================================
-
 const API_BASE = "https://script.google.com/macros/s/AKfycbxy-HT-J3pU66MHdTzGmBqVIuECtr7sWLscMArFm8STQh3QGiLzjV9ohMxnu8FDLAJW/exec"; // ← paste your /exec URL here
 
 let currentUser = null;
